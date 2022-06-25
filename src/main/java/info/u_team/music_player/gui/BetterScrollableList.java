c864f8bb2adf8eb20789e4f801b87dca3f8f493d
KingCreator11 <KingCreator11@gmail.com>
2022-06-25 02:31:57 -0400

    Fixed OSX Scroll Bug

--- src/main/java/info/u_team/music_player/gui/BetterScrollableList.java
+++ src/main/java/info/u_team/music_player/gui/BetterScrollableList.java
@@ -8,8 +8,8 @@ public class BetterScrollableList<T extends ObjectSelectionList.Entry<T>> extend
 	
 	public BetterScrollableList(int width, int height, int top, int bottom, int left, int right, int slotHeight, int sideDistance) {
 		super(width, height, top, bottom, left, right, slotHeight, sideDistance);
-		setRenderTopAndBottom(false);
-		setUseScissor(true);
+		setRenderTopAndBottom(true);
+		setUseScissor(false);
 		setRenderTransparentBorder(true);
 	}
 	
